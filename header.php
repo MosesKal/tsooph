7c75f947a04bb5d43b1554748760923a7fc9803e
MosesK <mosesziongo@gmail.com>
2023-08-17 06:27:25 +0200

    php and html

---
+++ header.php
@@ -0,0 +1,16 @@
+
+  <head>  
+    <script src="../assets/js/color-modes.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.115.4">
+    <title>php</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/navbar-fixed/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="navbar-fixed.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
